cbf565501b5ee01184258dc844cd3bfd8cad87c3
Matt Steitle <matt.steitle@users.noreply.github.com>
2018-05-01 15:06:46 -0600

    Initial commit of react app and noop content

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <section id="wrapper">
+          <header class="pageHeader">
+            <h1 class="pageTitle">Steitle Family Network</h1>
+          </header>
+
+          <main class="content">
+            <p>Nothing to see here</p>
+          </main>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
